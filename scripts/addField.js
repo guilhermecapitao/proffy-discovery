438a9b336d70baf5f6f6bd31b88be99de01fe485
Guilherme Capitao <guilherme.capitao95@gmail.com>
2020-08-05 01:04:46 -0300

    feat: manipulaçao dos campos do formulario

---
+++ scripts/addField.js
@@ -0,0 +1,14 @@
+document.querySelector("#add-time")
+.addEventListener("click", cloneField)
+
+function cloneField() {
+  const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+  const fields = newFieldContainer.querySelectorAll('input')
+
+  fields.forEach(function(field) {
+    field.value = ""
+  });
+
+  document.querySelector('.schedule-items').appendChild(newFieldContainer)
+}
