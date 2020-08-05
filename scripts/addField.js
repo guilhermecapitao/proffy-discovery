d31e06aaec6927e759cac3b3bad74f67c5af16e3
Guilherme Capitão <guilherme.capitao95@gmail.com>
2020-08-05 09:47:43 -0300

    fix: recerencia ao id do schedule no script

--- scripts/addField.js
+++ scripts/addField.js
@@ -2,7 +2,7 @@ document.querySelector("#add-time")
 .addEventListener("click", cloneField)
 
 function cloneField() {
-  const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+  const newFieldContainer = document.querySelector('#schedule-item').cloneNode(true)
 
   const fields = newFieldContainer.querySelectorAll('input')
 
